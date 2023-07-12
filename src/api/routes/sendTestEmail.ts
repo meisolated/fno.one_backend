fe876e1a0b02fc29f4a3326ec0614c26e87ea7dd
meisolated <isolatedbot@gmail.com>
2023-07-12 14:14:37 +0530

    minor changes

--- src/api/routes/sendTestEmail.ts
+++ src/api/routes/sendTestEmail.ts
@@ -4,7 +4,7 @@ import logger from "../../logger"
 export default async function (app: Express, path: string) {
     logger.info("Loaded route: " + path)
     app.get(path, async (req: Request, res: Response) => {
-        sendTestMail()
+        // sendTestMail()
         return res.send({ message: "Success", code: 200 })
     })
 }
